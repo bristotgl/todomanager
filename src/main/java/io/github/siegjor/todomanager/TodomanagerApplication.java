de753b6f551b1704ff31c1d1cfa3f69ff035d8ed
gabrielloli <bristotgl@gmail.com>
2025-01-23 10:18:48 -0300

    first commit

---
+++ src/main/java/io/github/siegjor/todomanager/TodomanagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.siegjor.todomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/siegjor/todomanager/TodomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.siegjor.todomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
