21c4128b03975b6fffffd4e4c80cfd3dcf880b73
gabrielloli <bristotgl@gmail.com>
2025-01-30 06:35:13 -0300

    add saving test for customer repository

---
+++ src/test/java/io/github/siegjor/todomanager/customer/CustomerRepositoryTest.java
@@ -0,0 +1,33 @@
+package io.github.siegjor.todomanager.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+public class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    public void shouldSaveAndFetchCustomer() {
+        Customer customer = new Customer();
+        customer.setUsername("Dalinar");
+        customer.setPassword("thewayofkings");
+
+        Customer savedCustomer = customerRepository.save(customer);
+        Optional<Customer> fetchedCustomer = customerRepository.findById(savedCustomer.getCustomerId());
+
+        assertThat(fetchedCustomer).isPresent();
+        assertThat(fetchedCustomer.get().getUsername()).isEqualTo("Dalinar");
+        assertThat(fetchedCustomer.get().getPassword()).isEqualTo("thewayofkings");
+        assertThat(fetchedCustomer.get().getCreatedAt()).isNotNull();
+    }
+}
